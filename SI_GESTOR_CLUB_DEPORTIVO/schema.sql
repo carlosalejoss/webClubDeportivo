bb7fc12d46f2dab2d77487ad86d02c8ad60826c3
carlosalejoss <872342@unizar.es>
2024-11-08 20:15:30 +0100

    update: make telefono field mandatory in usuario table for data integrity

--- SI_GESTOR_CLUB_DEPORTIVO/schema.sql
+++ SI_GESTOR_CLUB_DEPORTIVO/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS usuario (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(150) NOT NULL,
-    telefono VARCHAR(15),
+    telefono VARCHAR(15) NOT NULL,
     dni VARCHAR(15) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
