8045e9a359360d9efaeccd4db991df25fb9241b9
carlosalejoss <872342@unizar.es>
2024-11-27 19:53:03 +0100

    horarios de una clase se muestran en orden

--- SI_GESTOR_CLUB_DEPORTIVO/src/main/java/org/example/si_gestor_club_deportivo/repository/HorarioRepository.java
+++ SI_GESTOR_CLUB_DEPORTIVO/src/main/java/org/example/si_gestor_club_deportivo/repository/HorarioRepository.java
@@ -1,13 +1,15 @@
 package org.example.si_gestor_club_deportivo.repository;
 
 import org.example.si_gestor_club_deportivo.model.Horario;
-import org.example.si_gestor_club_deportivo.model.Pista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface HorarioRepository extends JpaRepository<Horario, Long> {
-    List<Horario> findByClaseId(Long claseId);
+
+    @Query("SELECT h FROM Horario h WHERE h.clase.id = :claseId ORDER BY h.fecha ASC, h.horaInicio ASC")
+    List<Horario> findByClaseIdOrderByFechaAndHoraInicio(@Param("claseId") Long claseId);
+
 }
